f4d41cbc3a4dedfdde99426c7452288b74f4c95a
WtoJglg4 <glazovv81@gmail.com>
2024-03-16 00:04:09 +0300

    feat: add hadnler

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	filmlib "github/film-lib"
+	handler "github/film-lib/pkg/handlers"
 	"log"
 )
 
 func main() {
 	srv := new(filmlib.Server)
+	mux := handler.NewHandler()
 
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", mux.InitRoutes()); err != nil {
 		log.Fatalf("error while running http server: %s\n", err.Error())
 	}
 }
