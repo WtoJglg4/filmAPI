cbc1a69e5a621ceec67d4c8e3ffe02cafc111ce8
WtoJglg4 <glazovv81@gmail.com>
2024-03-17 15:14:12 +0300

    feat: add db-tags to structs

--- filmapi.go
+++ filmapi.go
@@ -1,18 +1,19 @@
 package filmapi
 
 type Actor struct {
-	Id        int    `json:"-" db:"id"`
+	Id        int    `json:"id" db:"id"`
 	Name      string `json:"name" bindig:"required" db:"name"`
 	Gender    string `json:"gender" bindig:"required" db:"gender"`
 	BirthDate string `json:"birth_date" bindig:"required" db:"birth_date"`
 }
 
 type Film struct {
-	Id          int    `json:"-" db:"id"`
-	Name        string `json:"name" bindig:"required" db:"name"`
-	Description string `json:"description" bindig:"required" db:"description"`
-	ReleaseDate string `json:"release_date" bindig:"required" db:"release_date"`
-	Rating      int    `json:"rating" bindig:"required" db:"rating"`
+	Id          int      `json:"id" db:"id"`
+	Name        string   `json:"name" bindig:"required" db:"name"`
+	Description string   `json:"description" bindig:"required" db:"description"`
+	ReleaseDate string   `json:"release_date" bindig:"required" db:"release_date_new"`
+	Rating      int      `json:"rating" bindig:"required" db:"rating"`
+	Actors      []string `json:"actors" db:"actors_list"`
 }
 
 type ActorWithFilms struct {
